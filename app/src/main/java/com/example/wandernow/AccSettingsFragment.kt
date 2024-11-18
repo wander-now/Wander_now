f1493cad7da8f3ebe5fcc10408b4de1db28d6b1a
teqia <wordbird42@gmail.com>
2024-11-18 17:17:38 +0900

    .

--- app/src/main/java/com/example/wandernow/AccSettingsFragment.kt
+++ app/src/main/java/com/example/wandernow/AccSettingsFragment.kt
@@ -5,55 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.wandernow.databinding.FragmentAccSettingsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AccSettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AccSettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    var binding: FragmentAccSettingsBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_acc_settings, container, false)
+        binding = FragmentAccSettingsBinding.inflate(inflater)
+
+        return binding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AccSettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AccSettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.button1?.setOnClickListener {
+            findNavController().navigate(R.id.action_accSettingsFragment_to_securityFragment)
+        }
+        binding?.button2?.setOnClickListener {
+            findNavController().navigate(R.id.action_accSettingsFragment_to_personalInfoFragment)
+        }
+        binding?.button3?.setOnClickListener {
+            findNavController().navigate(R.id.action_accSettingsFragment_to_myReviewsFragment)
+        }
     }
+
 }
