b533b962cb2714c6d3a7298f8d1fcbb8dfad4e0f
james <jamesjang0622@naver.com>
2024-12-02 22:57:53 +0900

    junyoung_triplist_fragment_itemRecord_kt_adapter_updateVersion

--- app/src/main/java/com/example/wandernow/TripListFragment.kt
+++ app/src/main/java/com/example/wandernow/TripListFragment.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wandernow.databinding.FragmentTriplistBinding
+//import com.example.wandernow.dataclass.Location
+//import com.example.wandernow.dataclass.Triplist
+//import com.example.wandernow.homefragment.LocationRVAdapter
 
 class TripListFragment :Fragment(){
     lateinit var binding: FragmentTriplistBinding
@@ -21,7 +25,15 @@ class TripListFragment :Fragment(){
 
         binding = FragmentTriplistBinding.inflate(inflater,container,false)
 
+        binding.addListBtn.setOnClickListener{
+            Toast.makeText(requireContext(),"Button Clicked!!", Toast.LENGTH_SHORT).show()
+        }
+
+
         triplistDatas.apply{
+            add(Triplist("2024.10.21",R.drawable.img_detail_gapyeong_2,"장소: 경기도 가평군"))
+            add(Triplist("2024.10.02",R.drawable.gapyeoung2,"장소: 경기도 가평군"))
+            add(Triplist("2024.09.18",R.drawable.gapyeoung3,"장소: 경기도 가평군"))
             add(Triplist("2024.10.21",R.drawable.img_detail_gapyeong_2,"장소: 경기도 가평군"))
             add(Triplist("2024.10.02",R.drawable.gapyeoung2,"장소: 경기도 가평군"))
             add(Triplist("2024.09.18",R.drawable.gapyeoung3,"장소: 경기도 가평군"))
@@ -31,6 +43,19 @@ class TripListFragment :Fragment(){
         binding.tripRV.adapter = triplistRVAdapter
         binding.tripRV.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
 
+        triplistRVAdapter.setMyItemCLickListener(object: TriplistRVAdapter.MyItemClickListener {
+            override fun onItemClick(triplist: Triplist) {
+                changeTripDetailFragment(triplist)
+            }
+        })
+
         return binding.root
+    }
+
+    private fun changeTripDetailFragment(triplist: Triplist) {
+        (context as MainActivity).supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, TripDetailFragment().apply {
+            })
+            .commitAllowingStateLoss()
     }
 }
