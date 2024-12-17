6bebcd1161f36f503c5fcb41d0415b7e3f2a35e0
정성훈 <modestnature@naver.com>
2024-12-17 09:55:46 +0900

    complete

---
+++ app/src/main/java/com/example/wandernow/viewmodel/TripListViewModel.kt
@@ -0,0 +1,13 @@
+//package com.example.wandernow.viewmodel
+//
+//import androidx.lifecycle.LiveData
+//import com.example.wandernow.dataclass.Triplist
+//import com.example.wandernow.repository.TripListRepository
+//
+//class TripListViewModel {
+//    private val repository = TripListRepository()
+//
+//    fun getTripList(): LiveData<List<Triplist>> {
+//        return repository.getTripList()
+//    }
+//}
